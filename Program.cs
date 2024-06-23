a2468c6a46857f509d0c9b71047658b48371aba3
marginalfriend <abdullahpsg@gmail.com>
2024-06-23 10:57:56 +0700

    [INITIAL] initial submission

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PSBB
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input the number of families: ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Input the number of members in the family (separated by a space): ");
+            string[] input = Console.ReadLine().Split(' ');
+
+            if (input.Length != n)
+            {
+                Console.WriteLine("Input must be equal with count of family");
+                return;
+            }
+
+            List<int> familyMembers = input.Select(int.Parse).ToList();
+            int busesRequired = CalculateMinimumBuses(familyMembers);
+
+            Console.WriteLine($"Minimum bus required is: {busesRequired}");
+        }
+
+        static int CalculateMinimumBuses(List<int> familyMembers)
+        {
+            familyMembers.Sort();
+            int buses = 0;
+
+            int i = 0;
+            int j = familyMembers.Count - 1;
+
+            while (i <= j)
+            {
+                if (familyMembers[i] + familyMembers[j] <= 4)
+                {
+                    i++;
+                }
+                j--;
+                buses++;
+            }
+
+            return buses;
+        }
+    }
+}
